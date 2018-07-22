3ad6ea4772e203752592488e09c52c47caf6c32c
udugam <ryan.udugam@gmail.com>
2018-07-22 15:17:02 -0400

    communicating with twitter api

--- liri.js
+++ liri.js
@@ -1,1 +1,18 @@
-require("dotenv").config();
+//Importing environment variables and api keys
+require('dotenv').config();
+var keys = require('./keys.js');
+
+//Importing node modules
+var requestModule = require('request')
+var twitterModule = require('twitter')
+var spotifyModule = require('node-spotify-api')
+
+//Declaring Twitter and Spotify Objects 
+var twitter = new twitterModule(keys.twitter)
+// var spotify = new spotifyModule(keys.spotify)
+
+twitter.get('favorites/list', function(error, tweets, response) {
+    if(!error)  {
+        console.log(tweets);
+    };
+  });
