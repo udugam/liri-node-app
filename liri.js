9fd17ca6ca49eff314b59177c015bceedfc3760e
udugam <ryan.udugam@gmail.com>
2018-07-22 19:56:58 -0400

    spotify-this-song command working

--- liri.js
+++ liri.js
@@ -9,11 +9,36 @@ var spotifyModule = require('node-spotify-api')
 
 //Declaring Twitter and Spotify Objects 
 var twitter = new twitterModule(keys.twitter)
-// var spotify = new spotifyModule(keys.spotify)
+var spotify = new spotifyModule(keys.spotify)
 
-var params = {screen_name: 'Leo60747218'};
-twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-        console.log(tweets);
-    }
-});
+
+switch(process.argv[2]) {
+    case 'my-tweets':
+        twitter.get('statuses/user_timeline', {screen_name: 'Leo60747218'}, function(error, tweets, response) {
+            if (!error) {
+                tweets.forEach(function(element) {
+                    console.log(element.text);
+                }) 
+            }
+        });
+        break
+    
+    case 'spotify-this-song':
+        //Take the process.argv array, remove the first three elements and a make a string of the rest to account for spaces in the song name
+        var commandArray = process.argv
+        var songName = commandArray.slice(3).join(' ');
+    
+        spotify.search({ type: 'track', query: songName, limit:1 }, function(err, data) {
+            if (!err) {
+                var resultsList = data.tracks.items
+                resultsList.forEach( function(element) {
+                    console.log('Artist(s): ', element.album.artists[0].name); 
+                    console.log('Song Name: ', element.name);
+                    console.log('Preview Link: ', element.external_urls.spotify);                     
+                    console.log('Album: ', element.album.name);                                     
+                })
+            }
+        });
+        break
+}
+
