95a99c475509e5ecadc3a6c37fa89836e71f9eec
udugam <ryan.udugam@gmail.com>
2018-07-22 20:41:17 -0400

    added movie-this command

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 var keys = require('./keys.js');
 
 //Importing node modules
-var requestModule = require('request')
+var request = require('request')
 var twitterModule = require('twitter')
 var spotifyModule = require('node-spotify-api')
 
@@ -40,5 +40,30 @@ switch(process.argv[2]) {
             }
         });
         break
-}
+    
+    case 'movie-this':
+        //Take the process.argv array, remove the first three elements and a make a string of the rest to account for spaces in the movie name
+        var commandArray = process.argv
+        var movieName = commandArray.slice(3).join(' ');
+
+        //Declare and build query url request to the OMDB API
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+        
+        // Then create a request to the queryUrl
+        request(queryUrl,function(error,response,body) {
+            // If the request is successful
+            if(!error && response.statusCode=='200') {
+                // Then log the Release Year for the movie
+                console.log('Title: ', JSON.parse(body).Title)
+                console.log('Year: ', JSON.parse(body).Year)
+                console.log('IMDB Rating: ', JSON.parse(body).imdbRating)
+                console.log('Roten Tomatoes Rating: ', JSON.parse(body).Ratings[1].Value)
+                console.log('Country: ', JSON.parse(body).Country)
+                console.log('Language: ', JSON.parse(body).Language)
+                console.log('Plot: ', JSON.parse(body).Plot)
+                console.log('Actors: ', JSON.parse(body).Actors)
+            }
+        })
+    }
+
 
